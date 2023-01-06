8577e997f873964368e03bf6ea71681f6fd80024
Monica Portillo <44446125+monicaps@users.noreply.github.com>
2023-01-06 01:06:09 -0600

    regex dot

--- js/examples.js
+++ js/examples.js
@@ -1,7 +1,13 @@
+/*Utilización de Regex con Javascript de distintas maneras*/
+
 /*Expresión Regex que busca dentro del texto aquellas coincidencias
  donde se encuentre la palabra*/
 var text ="I have no special talents. I am only passionately curious.";
 let regex=/curious/gm;
-
 var result = text.match(regex);
 console.log(`El resultado es: ${result}`);
+
+var testText="abcABC123 .:!?";
+let expRegex=/./g;
+var resultRegex = text.match(expRegex);
+console.log(`El resultado es: ${resultRegex}`);
